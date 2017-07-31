c3686fb00a4cad1a1ad4d21444ba3528812045b3
Bradley_Carson <bmcarson1671@gmail.com>
2017-07-31 16:34:37 -0400

    initial commit

---
+++ main.js
@@ -0,0 +1,135 @@
+// The Form Data
+let formData = [
+  // For demonstration purposes,
+  // the first form element has been added to the HTML file as a comment
+  // compare the input in the HTML file with the contents of this first object
+  {
+    "type": "text",
+    "label": "First Name",
+    "id": "user-first-name",
+    "icon": "fa-user",
+    "options": []
+  },
+  {
+    "type": "text",
+    "label": "Last Name",
+    "id": "user-last-name",
+    "icon": "fa-user",
+    "options": []
+  },
+  {
+    "type": "email",
+    "label": "Email Address",
+    "id": "user-email",
+    "icon": "fa-envelope",
+    "options": []
+  },
+  {
+    "type": "text",
+    "label": "Current Website URL",
+    "id": "user-website",
+    "icon": "fa-globe",
+    "options": []
+  },
+  {
+    "type": "select",
+    "label": "Select Language",
+    "id": "user-language",
+    "icon": "",
+    "options": [
+      {
+        "label": "English",
+        "value": "EN"
+      },
+      {
+        "label": "French",
+        "value": "FR"
+      },
+      {
+        "label": "Spanish",
+        "value": "SP"
+      },
+      {
+        "label": "Chinese",
+        "value": "CH"
+      },
+      {
+        "label": "Japanese",
+        "value": "JP"
+      }
+    ]
+  },
+  {
+    "type": "textarea",
+    "label": "Your Comment",
+    "id": "user-comment",
+    "icon": "fa-comments",
+    "options": []
+  },
+  {
+    "type": "tel",
+    "label": "Mobile Number",
+    "id": "user-mobile",
+    "icon": "fa-mobile-phone",
+    "options": []
+  },
+  {
+    "type": "tel",
+    "label": "Home Number",
+    "id": "user-phone",
+    "icon": "fa-phone",
+    "options": []
+  }
+];
+
+// HINTS:
+// As you can see, we access the first element in the array
+// with [0] and then grab the property "label" using the "." operator
+( function(){
+  // Select the first element from the array
+  let first = formData[ 0 ];
+  // Log the first object
+  console.log( first );
+  // Log the string "First Name"
+  console.log( first.label );
+} )();
+
+
+// -------- Your Code Goes Below this Line --------
+
+var mainContainer = document.querySelector('.fields');
+
+//for loop
+for (i = 0; i < formData.length; i++) {
+
+//if statement to convert text or tel to input fields
+if (formData[i].type === 'text' || formData[i].type === 'tel' || formData[i].type === 'email') {
+  formData[i].type = 'input';
+}
+
+
+var newInput = document.createElement(formData[i].type);
+var optionsSelect = formData[i].options;
+var backgroundImageFa = formData[i].icon; //might not need
+
+//dropdown menu
+for (x = 0; x < optionsSelect.length; x++) {
+  var label = optionsSelect[x].label;
+  var value = optionsSelect[x].value;
+  var option = document.createElement('option');
+  newInput.appendChild(option);
+  option.textContent = label;
+  option.value = value;
+}
+//creating forms
+newInput.placeholder = formData[i].label;
+
+mainContainer.appendChild(newInput);
+
+var createImage = document.createElement('i');
+
+newInput.appendChild(createImage);
+
+
+
+}
